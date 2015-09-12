8bf7633d30ca43c463dabbfb6363ee69a9bd6211
Sean Grove <s@bushi.do>
2015-09-11 17:01:04 -0700

    Separating demo

--- profiles.clj
+++ profiles.clj
@@ -1,35 +1,35 @@
-{:dev     {:source-paths ["src/cljs" "env/dev/clj"]
+{:dev     {:source-paths ["env/dev/cljs/" "src/cljs" "env/dev/clj"]
 
            :plugins      [[cider/cider-nrepl "0.9.1" :exclusions [org.clojure/tools.nrepl]]
-                          [lein-ancient "0.6.7"]
-                          [lein-figwheel "0.3.3" :exclusions [org.clojure/clojure org.codehaus.plexus/plexus-utils compojure]]
+                          [lein-ancient "0.6.7" :exclusions [org.clojure/clojure]]
+                          [lein-figwheel "0.3.9-SNAPSHOT" :exclusions [org.clojure/clojure org.codehaus.plexus/plexus-utils compojure commons-codec org.clojure/tools.reader org.clojure/clojurescript]]
                           [refactor-nrepl "1.0.5" :exclusions [org.clojure/clojure org.clojure/tools.nrepl]]]
 
            :dependencies [[com.cemerick/piggieback "0.2.1" :exclusions [org.clojure/clojure org.clojure/clojurescript org.clojure/tools.nrepl]]
-                          [figwheel-sidecar "0.3.3" :exclusions [org.clojure/clojure commons-codec org.clojure/clojurescript org.clojure/tools.reader org.codehaus.plexus/plexus-utils org.clojure/tools.namespace medley clout compojure]]
+                          [figwheel-sidecar "0.3.9-SNAPSHOT" :exclusions [ring/ring-core org.clojure/clojure org.slf4j/log4j-over-slf4j org.codehaus.plexus/plexus-utils compojure commons-codec org.clojure/tools.reader org.clojure/clojurescript]]
                           [org.clojure/tools.nrepl "0.2.10"]
-                          [weasel "0.7.0-SNAPSHOT" :exclusions [http-kit org.clojure/clojurescript]]]
+                          [weasel "0.7.0" :exclusions [http-kit org.clojure/clojurescript]]]
 
-           :repl-options {:init-ns          dato.server
+           :repl-options {:init-ns          dato.lib.server
                           :nrepl-middleware [cemerick.piggieback/wrap-cljs-repl]}
            
            :test-paths   ["test/clj"]
            
-           :figwheel     {:http-server-root "public"
-                          :server-port      3449
-                          :nrepl-port       7888
-                          :css-dirs         ["resources/public/css"]}
-
            :env          {:is-dev true}}
 
- :uberjar {:source-paths ["env/prod/clj"]
-           :hooks        [leiningen.cljsbuild]
-           :env          {:is-dev false
+ :uberjar {:source-paths ["env/production/clj" "env/dev/cljs/"]
+           :prep-tasks   ["compile" ["cljsbuild" "once"]]
+           :env          {:is-dev     false
                           :production true}
            :omit-source  true
            :aot          :all
+           :plugins      [[cider/cider-nrepl "0.9.1" :exclusions [org.clojure/tools.nrepl]]
+                          [lein-ancient "0.6.7" :exclusions [org.clojure/clojure]]
+                          [refactor-nrepl "1.0.5" :exclusions [org.clojure/clojure org.clojure/tools.nrepl]]]
+
+           :dependencies [[org.clojure/tools.nrepl "0.2.10"]]
            :cljsbuild    {:builds [{:app
-                                     {:source-paths ["env/prod/cljs"]
-                                      :compiler
-                                      {:optimizations :advanced
-                                       :pretty-print  false}}}]}}}
+                                    {:source-paths ["env/production/cljs"]
+                                     :compiler
+                                     {:optimizations :advanced
+                                      :pretty-print  false}}}]}}}
--- project.clj
+++ project.clj
@@ -8,8 +8,6 @@
 
   :repositories {"my.datomic.com" {:url   "https://my.datomic.com/repo"
                                    :creds :gpg}}
-  :plugins [[lein-cljsbuild "1.0.6" :exclusions [org.clojure/clojurescript]]
-            [lein-environ "1.0.0"]]
 
   :pedantic? :abort
 
@@ -18,35 +16,23 @@
                [org.slf4j/log4j-over-slf4j]]
 
   :dependencies [ ;; Chrome extension for cljs dev
-                 [binaryage/devtools "0.3.0"]
                  [cheshire "5.5.0" :exclusions [com.fasterxml.jackson.core/jackson-core]]
                  [clj-http "1.1.2" :exclusions [com.fasterxml.jackson.dataformat/jackson-dataformat-smile com.fasterxml.jackson.core/jackson-core]]
-
-                 ;; Internal conflict with xml-apis versions, be sure to update both when you
-                 ;; update clj-pdf
-                 [clj-pdf "2.1.0" :exclusions [xml-apis]]
-                 [xml-apis "1.3.04"]
 
                  [cljsjs/react "0.12.2-8"]
                  [com.cognitect/transit-clj "0.8.275" :exclusions [com.fasterxml.jackson.core/jackson-core]]
                  [com.cognitect/transit-cljs "0.8.220" :exclusions [org.clojure/clojurescript]]
-                 [com.datomic/datomic-pro "0.9.5153" :exclusions [org.slf4j/slf4j-nop joda-time org.slf4j/slf4j-api org.clojure/clojurescript]]
+                 [com.datomic/datomic-pro "0.9.5206" :exclusions [org.slf4j/slf4j-nop joda-time org.slf4j/slf4j-api org.clojure/clojurescript]]
                  [com.fasterxml.jackson.core/jackson-annotations "2.3.1"]
                  [com.fasterxml.jackson.core/jackson-core "2.3.1"]
-                 [compojure "1.3.1"]
-                 ;;[datascript "0.11.6"]
-                 [environ "1.0.0"]
-                 [hiccup "1.0.5"]
-                 [instaparse "1.4.0"]
-                 [markdown-clj "0.9.66"]
                  [org.omcljs/om "0.8.8"]
                  [org.clojure/clojure "1.7.0-RC2"]
-                 [org.clojure/clojurescript "0.0-3308" :exclusions [org.clojure/tools.reader org.clojure/data.json]]
+                 [org.clojure/clojurescript "1.7.122" :exclusions [org.clojure/clojure org.clojure/tools.reader org.clojure/clojurescript
+                                                                   org.clojure/google-closure-library-third-party]]
                  [org.clojure/core.async "0.1.346.0-17112a-alpha"]
                  [org.clojure/data.json "0.2.6" :classifier "aot"]
-                 [org.clojure/google-closure-library "0.0-20150505-021ed5b3"]
+                 [org.clojure/google-closure-library "0.0-20150805-acd8b553" :exclusions [org.clojure/google-closure-library-third-party org.clojure/clojure org.clojure/clojurescript]]
                  [org.clojure/google-closure-library-third-party "0.0-20150505-021ed5b3"]
-                 [org.clojure/test.check "0.7.0"]
                  [org.clojure/tools.logging "0.2.6"]
                  [log4j "1.2.17"]
                  [log4j/apache-log4j-extras "1.1"]
@@ -64,54 +50,7 @@
                                                         ch.qos.logback/logback-classic]]
                  [org.immutant/immutant-transit "0.2.3" :exclusions [com.fasterxml.jackson.core/jackson-core]]
                  [org.postgresql/postgresql "9.3-1102-jdbc41"]
-                 [org.postgresql/postgresql "9.3-1102-jdbc41"]
-                 [pdfboxing "0.1.5" :exclusions [log4j/log4j]]
-                 [precursor/om-i "0.1.7"]
-                 [prismatic/om-tools "0.3.11" :exclusions [om]]
-                 [prismatic/schema "0.4.3"]
-                 [racehub/om-bootstrap "0.5.1"]
-                 [ring "1.3.2"]
-                 [ring/ring-defaults "0.1.3"]
-                 [sablono "0.3.4"]
                  [org.clojure/tools.nrepl "0.2.10"]]
-
-  :cljsbuild {:test-commands {"test" ["phantomjs" "env/test/js/unit-test.js" "env/test/unit-test.html"]}
-              :builds        [{:id           "dev"
-                               :source-paths ["src/cljs" "src/shared/" "yaks/datascript/src" "yaks/datascript/bench/src"]
-                               :figwheel     true
-                               :compiler     {:asset-path    "/js/bin-debug"
-                                              :main          dato.client.core
-                                              :output-to     "resources/public/js/bin-debug/main.js"
-                                              :output-dir    "resources/public/js/bin-debug/"
-                                              :optimizations :none
-                                              :pretty-print  true
-                                              :preamble      ["react/react.js"]
-                                              :externs       ["react/externs/react.js"]
-                                              :source-map    true
-                                              :warnings      {:single-segment-namespace false}}}
-                              {:id "test"
-                               :source-paths ["src/cljs" "test/cljs" "yaks/datascript/src" "yaks/datascript/bench/src"]
-                               :compiler {:pretty-print  true
-                                          :output-to     "resources/public/cljs/test/frontend-dev.js"
-                                          :output-dir    "resources/public/cljs/test"
-                                          :optimizations :advanced
-                                          :externs       ["datascript/externs.js"]
-                                          :source-map    "resources/public/cljs/test/sourcemap-dev.js"
-                                          :warnings      {:single-segment-namespace false}}}
-                              {:id "production"
-                               :source-paths ["src/cljs" "src/shared/" "yaks/datascript/src" "yaks/datascript/bench/src"]
-                               :compiler     {:asset-path    "/js/bin"
-                                              :main          dato.client.core
-                                              :output-to     "resources/public/js/bin/main.js"
-                                              :output-dir    "resources/public/js/bin/"
-                                              :optimizations :advanced
-                                              :pretty-print  false
-                                              :preamble      ["react/react.js"]
-                                              :externs       ["react/externs/react.js"]
-                                              :warnings      {:single-segment-namespace false}}
-                               :jar true}]}
-
-  :main ^:skip-aot dato.init
 
   :source-paths ["src/" "src/shared/" "yaks/datascript/src" "yaks/datascript/bench/src"]
 
